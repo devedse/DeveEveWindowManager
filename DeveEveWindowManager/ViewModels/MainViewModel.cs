df0902dbc193b39ac262de23335610a6a64c4a54
Devedse <davydavidse@gmail.com>
2024-12-01 19:14:24 +0100

    Added functionality

--- DeveEveWindowManager/ViewModels/MainViewModel.cs
+++ DeveEveWindowManager/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia;
+using Avalonia.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using DeveEveWindowManager.Models;
@@ -14,7 +15,7 @@ namespace DeveEveWindowManager.ViewModels;
 public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
-    private string _greeting = "Welcome to DeveEveWindowManager, please click on Reload screens first";
+    private string _greeting = "Welcome to DeveEveWindowManager, please send any donations to Devedse (ingame)";
     private readonly ScreenService? _screenService;
     private readonly WindowService? _windowService;
 
@@ -30,6 +31,8 @@ public partial class MainViewModel : ViewModelBase
 
     public double RelativeWidthComparedToHeight => Screens.Count == 0 ? 1 : (double)Screens.Max(t => t.OriginalBounds.X + t.OriginalBounds.Width) / Screens.Max(t => t.OriginalBounds.Y + t.OriginalBounds.Height);
 
+    private DispatcherTimer _disTimer = new DispatcherTimer();
+
     public MainViewModel()
     {
         //Design time
@@ -43,12 +46,22 @@ public partial class MainViewModel : ViewModelBase
 
     public MainViewModel(ScreenService screenService, WindowService windowService)
     {
+        _disTimer.Interval = TimeSpan.FromSeconds(0);
+        _disTimer.Tick += (sender, e) => LoadAll();
+        _disTimer.Start();
+
         Screens.CollectionChanged += (sender, e) => OnPropertyChanged(nameof(RelativeWidthComparedToHeight));
 
         _screenService = screenService;
         _windowService = windowService;
     }
 
+    private void LoadAll()
+    {
+        LoadScreens();
+        LoadWindowInstances();
+        _disTimer.Interval = TimeSpan.FromSeconds(5);
+    }
 
     private void LoadScreens()
     {
@@ -76,7 +89,7 @@ public partial class MainViewModel : ViewModelBase
         }
 
         var selectedScreens = Screens.Where(t => t.IsSelected).ToList();
- 
+
         _windowService?.MoveAndResizeWindow(SelectedEveInstance, selectedScreens);
     }
 
